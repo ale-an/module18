15ed22b40ae9711a6b67157b1ff2552f3403c207
alena <vorozhtsova_alena@mail.ru>
2023-10-30 20:42:37 +0500

    Задание 18.1.6

--- Module18/Program.cs
+++ Module18/Program.cs
@@ -5,12 +5,13 @@
         static void Main(string[] args)
         {
             Task_18_1_1();
+            Task_18_1_6();
         }
 
         private static void Task_18_1_1()
         {
             var carPlant = new CarPlant();
-            
+
             var scooterConveyor = new ScooterConveyor();
             var carConveyor = new CarConveyor();
             var motoConveyor = new MotoConveyor();
@@ -22,6 +23,21 @@
             scooterConveyor.Product.Show();
             carConveyor.Product.Show();
             motoConveyor.Product.Show();
+        }
+
+        private static void Task_18_1_6()
+        {
+            BaseClass myObject = new ImplementationOne(1);
+            myObject.GetId();
+
+            BaseClass clone = myObject.Clone();
+            clone.GetId();
+
+            myObject = new ImplementationTwo(2);
+            myObject.GetId();
+
+            clone = myObject.Clone();
+            clone.GetId();
         }
     }
 }
