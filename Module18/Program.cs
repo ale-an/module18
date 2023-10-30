8f56ad7169375c0f8352915d3f6e9a3cf50f8ffa
alena <vorozhtsova_alena@mail.ru>
2023-10-30 19:12:29 +0500

    Задание 18.1.1

---
+++ Module18/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Module18
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_18_1_1();
+        }
+
+        private static void Task_18_1_1()
+        {
+            var carPlant = new CarPlant();
+            
+            var scooterConveyor = new ScooterConveyor();
+            var carConveyor = new CarConveyor();
+            var motoConveyor = new MotoConveyor();
+
+            carPlant.Construct(scooterConveyor);
+            carPlant.Construct(carConveyor);
+            carPlant.Construct(motoConveyor);
+
+            scooterConveyor.Product.Show();
+            carConveyor.Product.Show();
+            motoConveyor.Product.Show();
+        }
+    }
+}
